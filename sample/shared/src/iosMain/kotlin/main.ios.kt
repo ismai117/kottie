6914e0c1c27e94a4a4c6cafb31521bcd546c5452
ismail <ismail1973x@gmail.com>
2024-06-10 10:37:06 +0100

    Merge branch 'main' of https://github.com/ismai117/kottie

--- sample/shared/src/iosMain/kotlin/main.ios.kt
+++ sample/shared/src/iosMain/kotlin/main.ios.kt
@@ -1,7 +1,4 @@
-import androidx.compose.runtime.ExperimentalComposeApi
 import androidx.compose.ui.window.ComposeUIViewController
 
-
-
-@OptIn(ExperimentalComposeApi::class)
 fun MainViewController() = ComposeUIViewController() { App() }
+
